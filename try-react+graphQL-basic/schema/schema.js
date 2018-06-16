db482844af7147f4a8d54f3cabaef9ee9c70cdfd
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-06-15 17:24:51 -0700

    chore: add readme

---
+++ try-react+graphQL-basic/schema/schema.js
@@ -0,0 +1,34 @@
+const grapql = require('graphql');
+const axios = require('axios');
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} = grapql;
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/users/${args.id}`).then(res => res.data)
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
