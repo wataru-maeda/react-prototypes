734f44815cd1572ff02d0fa1ee2885ee296a81bc
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-06-15 17:09:07 -0700

    chore: install axios

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,16 +1,11 @@
 const grapql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
     GraphQLSchema
 } = grapql;
-
-const users = [
-    { id: '23', firstName: 'Jodan', age: 20 },
-    { id: '47', firstName: 'Sara', age: 21 }
-];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -28,7 +23,7 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: { id: { type: GraphQLString } },
             resolve(parentValue, args) {
-                return _.find(users, { id: args.id });
+                return axios.get(`http://localhost:3000/users/${args.id}`).then(res => res.data)
             }
         }
     }
