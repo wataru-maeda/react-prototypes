d610578d0ec8c52bb6c18512e89ebddefda56244
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-06-22 13:56:44 -0700

    chore: cleanup code

--- try-news/src/screens/home/Home.js
+++ try-news/src/screens/home/Home.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import logo from '../../assets/images/logo.svg';
-
-
 import './Home.css';
 
-export default class Home extends Component {
+export default class Home extends Component {  
   render() {
     return (
       <div className="container">
