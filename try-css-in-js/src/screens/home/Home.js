8e0a13505c4fbee88b2197767f6646e8d26c7135
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-07-25 12:45:11 -0700

    chore: media query practice

--- try-css-in-js/src/screens/home/Home.js
+++ try-css-in-js/src/screens/home/Home.js
@@ -5,12 +5,12 @@ import './Home.css';
 export default class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Home</h1>
+      <div className="Home">
+        <header className="Home-header">
+          <img src={logo} className="Home-logo" alt="logo" />
+          <h1 className="Home-title">Welcome to Home</h1>
         </header>
-        <p className="App-intro">
+        <p className="Home-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
