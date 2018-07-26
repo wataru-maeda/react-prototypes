172668b15b5e1005fc65182372a3984b5fdcdbcf
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-07-25 18:05:17 -0700

    chore: update layout

--- try-css-in-js/src/screens/home/Home.js
+++ try-css-in-js/src/screens/home/Home.js
@@ -4,29 +4,27 @@ import './Home.css';
 export default class Home extends Component {
   render() {
     return (
-      <div className="home-container">
-        <div className="home-header">
-          Header Header Header Header Header Header Header Header
-          Header Header Header Header Header Header Header Header
-          Header Header Header Header Header Header Header Header
+      <div className="container">
+        <div className="head">
+          Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header
+          Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header
+          Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header
+          </div>
+        <div className="left">
+          Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left
+          Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left
+          Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left Left
         </div>
-        <span className="home-left-menu">
-          Left Menu Left Menu Left Menu Left Menu Left Menu Left Menu
-          Left Menu Left Menu Left Menu Left Menu Left Menu Left Menu
-          Left Menu Left Menu Left Menu Left Menu Left Menu Left Menu
-          Left Menu Left Menu Left Menu Left Menu Left Menu Left Menu
-        </span>
-        <span className="home-contents">
-          Constens Constens Constens Constens Constens Constens Constens
-          Constens Constens Constens Constens Constens Constens Constens
-          Constens Constens Constens Constens Constens Constens Constens
-          Constens Constens Constens Constens Constens Constens Constens
-        </span>
-        <span className="home-right-menu">
-          Right Menu Right Menu Right Menu Right Menu Right Menu Right Menu
-          Right Menu Right Menu Right Menu Right Menu Right Menu Right Menu
-          Right Menu Right Menu Right Menu Right Menu Right Menu Right Menu
-        </span>
+        <div className="center">
+          Center Center Center Center Center Center Center Center Center Center Center Center Center Center Center
+          Center Center Center Center Center Center Center Center Center Center Center Center Center Center Center
+          Center Center Center Center Center Center Center Center Center Center Center Center Center Center Center
+        </div>
+        <div className="right">
+          Right Right Right Right Right Right Right Right Right Right Right Right Right Right Right Right Right Right
+          Right Right Right Right Right Right Right Right Right Right Right Right Right Right Right Right Right Right
+          Right Right Right Right Right Right Right Right Right Right Right Right Right Right Right Right Right Right
+        </div>
       </div>
     );
   }
