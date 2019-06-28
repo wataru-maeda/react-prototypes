9dc5cde4b3df8f38161f1d11fcc2456f2732f0bc
Wataru Maeda <wmaeda@WatarunoMacBook-Pro.local>
2019-06-27 23:52:05 -0700

    chore: add base project for react-transition-group

--- try-animations/src/screens/home/Home.js
+++ try-animations/src/screens/home/Home.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
+import ReactTransitionGroup from './ReactTransitionGroup'
 import { images } from '../../assets'
 
 const styles = {
   root: {
     display: 'flex',
+    flexDirection: 'column',
     flex: 1,
     padding: 50,
   }
 }
+
+// https://github.com/aholachek/react-animation-comparison
 
 export default class App extends Component {
   render() {
     return (
       <div style={styles.root}>
         <h1>React Animation Demos</h1>
+        <ReactTransitionGroup />
       </div>
     );
   }
---
+++ try-animations/src/screens/home/ReactTransitionGroup.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+const styles = {
+  root: {
+    display: 'flex',
+    flex: 1,
+    marginTop: 50,
+  },
+}
+
+class ReactTransitionGroup extends Component {
+  render() {
+    return (
+      <div style={styles.root}>
+        <h4>
+          <a href="https://github.com/reactjs/react-transition-group"># reactjs/react-transition-group</a>
+        </h4>
+      </div>
+    )
+  }
+}
+
+export default ReactTransitionGroup
