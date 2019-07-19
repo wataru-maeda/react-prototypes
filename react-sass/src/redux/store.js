5006a8519b7c1ec8c8bd8ce1629f732f4dd1d7a4
Wataru Maeda <wmaeda@WatarunoMacBook-Pro.local>
2019-07-18 23:38:08 -0700

    chore: add new project

---
+++ react-sass/src/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import promiseMiddleware from 'redux-promise-middleware'
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
+
+const middlewares = [promiseMiddleware(), thunk, createLogger()]
+// const middlewares = [promiseMiddleware(), thunk]
+
+export default createStore(reducers, undefined, compose(applyMiddleware(...middlewares)));
---
+++ react-sass/src/redux/types.js
@@ -0,0 +1,2 @@
+
+export const USER_NAME = 'USER_NAME';
